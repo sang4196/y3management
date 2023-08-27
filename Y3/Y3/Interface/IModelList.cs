a390bd444ef9e56910aa75ac7a90e2d5ff809199
LEE SANGHO <sang4196@naver.com>
2023-08-27 22:15:21 +0900

    refactoring.. (can't build)

--- Y3/Y3/Interface/IModelList.cs
+++ Y3/Y3/Interface/IModelList.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Y3.Models;
+using Y3.Utility.Enums;
 
 namespace Y3.Interface
 {
     public interface IModelList
     {
+        void ReadData();
+        DataTable GetDataTable();
+        T GetbyId<T>(int id);
+        List<T> GetList<T>();
+        void Add<T>(List<T> list);
+
+        void UpdateData<T>(T data, eDBQueryType type);
+        void Save<T>(T data, eDBQueryType type);
     }
 }
--- Y3/Y3/Models/BasicModelList.cs
+++ Y3/Y3/Models/BasicModelList.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Y3.Interface;
+using Y3.Utility.Enums;
 
 namespace Y3.Models
 {
-    public abstract class BasicModelList<T> : IModelList
+    public abstract class BasicModelList<T> : IModelList where T : class
     {
+        // abstract
+        public abstract void Add<T>(List<T> list);
+        public abstract T GetbyId<T>(int id);
+        public abstract DataTable GetDataTable();
+        public abstract List<T> GetList<T>();
+        public abstract void ReadData();
+        public abstract void Save<T>(T data, eDBQueryType type);
+        public abstract void UpdateData<T>(T data, eDBQueryType type);
+
     }
 }
